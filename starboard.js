63a2482750e66870ef27245c5ec506c2b4953ec1
Vick Torri <VickTorriVIP@gmail.com>
2018-11-18 18:10:07 -0800

    Flight Starboat :: https://discord.gg/8kSexNf

--- starboard.js
+++ starboard.js
@@ -1,107 +1,130 @@
-const { Client } = require('eris');
+const Eris = require('eris');
 const Database = require('better-sqlite3');
 const db = new Database('star.db');
-const client = new Client('abcdefg123456');
+const client = new Eris(process.env.TOKEN);
 
 db.prepare('CREATE TABLE IF NOT EXISTS starids (msgid TEXT PRIMARY KEY, starid TEXT NOT NULL)').run();
 
+client.on("ready", () => {
+  console.log(`Starboat has offically booted with no errors.`)
+});
+
 client.on('messageReactionAdd', async (message, emoji, user) => {
 
-    if (message.channel.type !== 0 || emoji.name !== '⭐') return;
+  if (message.channel.type !== 0 || emoji.name !== '⭐') return;
 
-    const channel = client.getChannel(message.channel.id);
-    const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
+  const channel = client.getChannel(message.channel.id);
+  const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
 
-    if (channel.nsfw || !starboard || channel.id === starboard.id) return;
+  if (channel.nsfw || !starboard || channel.id === starboard.id) return;
 
-    const msg = await channel.getMessage(message.id);
-    const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id).bot).length;
+  const msg = await channel.getMessage(message.id);
+  const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id).bot).length;
 
-    if (stars < 5) return;
+  if (stars < 5) return;
 
-    if (msg.content.length === 0 && msg.attachments.length === 0 && (!msg.embeds[0] || msg.embeds[0].type !== 'image')) return;
+  if (msg.content.length === 0 && msg.attachments.length === 0 && (!msg.embeds[0] || msg.embeds[0].type !== 'image')) return;
 
-    const starId = await getMessageFromDatabase(msg.id);
+  const starId = await getMessageFromDatabase(msg.id);
 
-    if (!starId) {
-        if (!stars) return;
+  if (!starId) {
+    if (!stars) return;
 
-        const starMsg = await starboard.createMessage({
-            content: `**__Starboard:__ ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!**`,
-            embed: {
-                color: 16775619,
-                footer: {
-                    icon_url: "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/star-512.png",
-                    text: `You're a star! | ${msg.id}`
+    const starMsg = await starboard.createMessage({
+        content: `**__Starboard:__ ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!**`,
+        embed: {
+          color: 16775619,
+          footer: {
+            icon_url: "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/star-512.png",
+            text: `You're a star! | ${msg.id}`
+          },
+          author: {
+            name: `${msg.author.username}#${msg.author.discriminator} - Starboard`,
+            icon_url: msg.author.avatarURL,
+          },
+          image: resolveAttachment(msg),
+          timestamp: new Date(),
+          fields: [
+            {
+              name: "Message Content",
+              value: `${msg.content || '**__Image: No Text.__**'}`
+            },
+            {
+              name: "Jump To Message",
+              value: `[Click Here](https://discordapp.com/channels/493152507414052867/${msg.channel.id}/${msg.id})`
                 },
-                author: {
-                    name: `${msg.author.username}#${msg.author.discriminator} - Starboard`,
-                    icon_url: msg.author.avatarURL,
-                },
-                image: resolveAttachment(msg),
-                timestamp: new Date(),
-                fields: [{
-                        name: "Message Content",
-                        value: `${msg.content || '**__Image: No Text.__**'}`
-                    },
-                    {
-                        name: "Jump To Message",
-                        value: `[Click Here](https://discordapp.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id})`
-                    },
-                ],
+              ],
             },
         });
+        
 
-        db.prepare('INSERT INTO starids VALUES (?, ?)').run(msg.id, starMsg.id);
-    } else {
-        const starMessage = await starboard.getMessage(starId);
-        if (!starMessage) return;
-        await starMessage.edit(`**__Starboard:__** ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!`);
-    }
+
+    db.prepare('INSERT INTO starids VALUES (?, ?)').run(msg.id, starMsg.id);
+  } else {
+    const starMessage = await starboard.getMessage(starId);
+    if (!starMessage) return;
+    await starMessage.edit(`**__Starboard:__** ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!`);
+  }
 });
 
 client.on('messageReactionRemove', async (message, emoji, user) => {
-    if (message.channel.type !== 0 || emoji.name !== '⭐') return;
+  if (message.channel.type !== 0 || emoji.name !== '⭐') return;
 
-    const channel = client.getChannel(message.channel.id);
-    const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
+  const channel = client.getChannel(message.channel.id);
+  const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
 
-    if (!starboard || channel.id === starboard.id) return;
+  if (!starboard || channel.id === starboard.id) return;
 
-    const msg = await channel.getMessage(message.id);
-    const starId = await getMessageFromDatabase(msg.id);
-    if (!starId) return;
+  const msg = await channel.getMessage(message.id);
+  const starId = await getMessageFromDatabase(msg.id);
+  if (!starId) return;
 
-    const starMessage = await starboard.getMessage(starId);
-    if (!starMessage) return;
+  const starMessage = await starboard.getMessage(starId);
+  if (!starMessage) return;
 
-    if (!msg.reactions['⭐']) {
-        db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
-        return await starMessage.delete();
-    }
+  if (!msg.reactions['⭐']) {
+    db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
+    return await starMessage.delete();
+  }
 
-    const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id).bot).length;
+  const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id).bot).length;
 
-    if (!stars) {
-        db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
-        return await starMessage.delete();
-    }
+  if (!stars) {
+    db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
+    return await starMessage.delete();
+  }
 
-    await starMessage.edit(`$**__Starboard:__** ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!`);
+  await starMessage.edit(`$**__Starboard:__** ${stars} ⭐ - <#${msg.channel.id}> ${msg.author.username}#${msg.author.discriminator} has made it!`);
 });
 
 function getMessageFromDatabase(msgid) {
-    return (db.prepare('SELECT * FROM starids WHERE msgid = ?').get(msgid) || {}).starid;
+  return (db.prepare('SELECT * FROM starids WHERE msgid = ?').get(msgid) || {}).starid;
 }
 
 function resolveAttachment(msg) {
-    if (msg.attachments.length > 0 && msg.attachments[0].width) {
-        return msg.attachments[0];
-    } else if (msg.embeds.length > 0 && msg.embeds[0].type === 'image') {
-        return msg.embeds[0].image || msg.embeds[0].thumbnail;
-    } else {
-        return null;
-    }
+  if (msg.attachments.length > 0 && msg.attachments[0].width) {
+    return msg.attachments[0];
+  } else if (msg.embeds.length > 0 && msg.embeds[0].type === 'image') {
+    return msg.embeds[0].image || msg.embeds[0].thumbnail;
+  } else {
+    return null;
+  }
 }
 
 client.connect();
+
+/* Uptime Robot
+/ - This can be removed if not hosted on Glitch
+- You can also use an external service for uptime */
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+    response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
